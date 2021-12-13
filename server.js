3c1a0c2924837dd64a32605fdca26def75725cb3
Jerzy Kubisiak <jerzy.kubisiak@gmail.com>
2021-12-13 18:45:32 +0100

    some progress

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
 const app = express()
 
+mongoose.connect('mongodb://localhost/blog')
+
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({ extended: false}))
 app.use('/articles', articleRouter)
 
 app.get('/', (req, res) => {
